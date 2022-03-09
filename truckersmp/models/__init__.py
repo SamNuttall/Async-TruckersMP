5c7788610bc146cb322d80de955b3dc14dcc78d8
Sam <nuttall.sam@gmail.com>
2022-03-09 17:05:38 +0000

    ci: add __init__.py to models

---
+++ truckersmp/models/__init__.py
@@ -0,0 +1,7 @@
+from .server import Server
+from .player import Player
+from .ban import Ban
+from .event import Event, Events, EventsAttributes
+from .vtc import VTC, VTCs, VTCsAttributes, NewsPost, Role, Member
+from .version import Version
+from .rules import Rules
