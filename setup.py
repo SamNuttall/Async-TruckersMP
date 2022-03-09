3a028d008c6e849c8298581a01754be2c7a5bf89
Sam <nuttall.sam@gmail.com>
2022-03-09 16:01:25 +0000

    ci: update requirements, add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async-truckersmp",
+    version="0.1",
+    description="An asynchronous Python API wrapper for TruckersMP.",
+    long_description=open("README.md", "r", encoding="UTF-8").read(),
+    url="https://github.com/SamNuttall/Async-TruckersMP",
+    author="Sam Nuttall",
+    author_email="nuttall.sam@gmail.com",
+    license="MIT",
+    keywords="truckersmp",
+    packages=find_packages(),
+    install_requires=open("requirements.txt", "r", encoding="UTF-8").read().strip().splitlines(),
+    extra_requires={
+        "readthedocs": open("requirements-docs.txt", "r", encoding="UTF-8").read().strip().splitlines()
+    },
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ]
+)
